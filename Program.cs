2fc45e591c77997e583937bf820782ce7d3e7f0b
Bernardo Ramos <BERNARDO31NEGRELO@GMAIL.COM>
2025-04-07 20:59:02 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class exercicio2
+{
+    static void Main()
+    {
+        Console.WriteLine("Informe o ano do seu nascimento: ");
+        int anonascimento = int.Parse(Console.ReadLine());
+        
+        int idade = 2024 - anonascimento;
+        
+        Console.WriteLine($"Sua idade é {idade}");
+}
+}
